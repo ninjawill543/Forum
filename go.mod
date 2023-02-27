96a2b85c4153db8c528285c1635382dca81d2655
flizze <alexandre.bel@ynov.com>
2023-02-27 15:13:24 +0100

    db update

--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module forum
-
 go 1.18
-
 require (
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
--- main/main.go
+++ main/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", Handler_index)
+
+	database, _ := sql.Open("sqlite3", "../forum.db")
+	createTable(database)
+	defer database.Close()
 
 	//url of our funcs
 	fs := http.FileServer(http.Dir("./static"))
@@ -18,16 +25,58 @@ func main() {
 }
 
 func Handler_index(w http.ResponseWriter, r *http.Request) {
+	database, _ := sql.Open("sqlite3", "../forum.db")
 	tmpl1 := template.Must(template.ParseFiles("../static/index.html"))
 
 	if r.Method == "POST" {
 		username := r.FormValue("input_username")
 		password := r.FormValue("input_password")
+		mail := r.FormValue("input_mail")
+		t := time.Now()
+		creationDate := t.Format("20060102150405")
+		birthDay := r.FormValue("input_birthDay")
+		notifications := r.FormValue("input_notifications")
 
-		fmt.Print(username, "\n")
-		fmt.Print(password, "\n")
-
+		addUsers(database, username, password, mail, creationDate, birthDay, notifications)
 	}
 
-	tmpl1.Execute(w, "babygirl")
+	tmpl1.Execute(w, "")
+}
+
+func addUsers(db *sql.DB, username string, password string, email string, creationDate string, birthDate string, notifications string) {
+	usersInfo := `INSERT INTO users(username, password, email, creationDate, birthDate, notifications) VALUES (?, ?, ?, ?, ?, ?)`
+	query, err := db.Prepare(usersInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = query.Exec(username, password, email, creationDate, birthDate, notifications)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("adding new user :", username, "in users")
+	}
+}
+
+func createTable(db *sql.DB) {
+	users_table := `CREATE TABLE users(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"username" TEXT,
+		"password" TEXT,
+		"email" TEXT,
+		"creationDate" TEXT,
+		"birthDate" TEXT,
+		"notifications" TEXT);`
+
+	fmt.Println("test")
+
+	query, err := db.Prepare(users_table)
+
+	fmt.Println("test2")
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		query.Exec()
+		fmt.Println("Table created successfully")
+	}
 }
