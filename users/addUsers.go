04e027270a84eeb6822cdf18bbe0e5be79155097
flizze <alexandre.bel@ynov.com>
2023-02-28 16:41:30 +0100

    now cant use already taken username or mail

--- users/addUsers.go
+++ users/addUsers.go
@@ -8,15 +8,41 @@ import (
 )
 
 func AddUsers(db *sql.DB, username string, password string, email string, creationDate time.Time, birthDate string) {
-	usersInfo := `INSERT INTO users(username, password, email, creationDate, birthDate) VALUES (?, ?, ?, ?, ?)`
-	query, err := db.Prepare(usersInfo)
+	var testBool bool
+	newUsername := username
+	newEmail := email
+	rows, err := db.Query("SELECT username, email FROM users;")
 	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = query.Exec(username, password, email, creationDate, birthDate)
-	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	} else {
-		fmt.Println("adding new user :", username, "in users")
+		for rows.Next() {
+			defer rows.Close()
+			err = rows.Scan(&username, &email)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				if username == newUsername {
+					fmt.Println("username already taken")
+					testBool = true
+				} else if email == newEmail {
+					fmt.Println("email already taken")
+					testBool = true
+				}
+			}
+		}
+		rows.Close()
+		if testBool == false {
+			usersInfo := `INSERT INTO users(username, password, email, creationDate, birthDate) VALUES (?, ?, ?, ?, ?)`
+			query, err := db.Prepare(usersInfo)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = query.Exec(newUsername, password, newEmail, creationDate, birthDate)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Println("adding new user :", newUsername, "in users")
+			}
+		}
 	}
 }
