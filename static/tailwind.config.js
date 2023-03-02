1ba2eca86d8b51c2bd7d99d1e8aebdc7fabfb6dd
ogb4n <ogb4n@protonmail.com>
2023-03-03 00:23:03 +0100

    MainPagePt1

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html/*.html"],
+  content: ["./html/*.html",],
   theme: {
     extend: {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        'color1': '#A8A7A7',
-        'color2': '#CC527A',
-        'color3': '#E8175D',
-        'color4': '#474747',
-        'color5': '#363636',
+        'color1': '#d3daf3',
+        'color2': '#9092fb',
+        'color3': '#f33862',
+        'color4': '#2C3639',
+        'color5': '#676afb',
+      backgroundImage: {
+        'frontend': "url('https://i.pinimg.com/originals/14/fc/d1/14fcd189633936157ee354b6a092169d.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+        }
       },
     },
   },
