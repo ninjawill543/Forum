20cec76de72e266ffc20792b70e1238d76be911c
m4ul <lucas.hanson@ynov.com>
2023-03-02 12:02:42 +0100

    updated topics

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./html/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'color1': '#A8A7A7',
+        'color2': '#CC527A',
+        'color3': '#E8175D',
+        'color4': '#474747',
+        'color5': '#363636',
+      },
+    },
   },
   plugins: [],
 }
