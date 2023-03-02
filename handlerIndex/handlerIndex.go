d3674a03baee04166ef633f0c8bdc6fc104f2772
flizze <alexandre.bel@ynov.com>
2023-03-02 12:30:43 +0100

    display topics on html

--- handlerIndex/handlerIndex.go
+++ handlerIndex/handlerIndex.go
@@ -8,17 +8,18 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/index", Handler_index)
-}
 func Handler_index(w http.ResponseWriter, r *http.Request) {
 	databaseUsers, _ := sql.Open("sqlite3", "../users.db")
 	databaseTopics, _ := sql.Open("sqlite3", "../topic.db")
+	if len(t.TOPICS) == 0 {
+		t.DisplayTopic(databaseTopics)
+	}
 	tmpl1 := template.Must(template.ParseFiles("../static/html/index.html"))
 
 	//register on specifig button
 	// if button (register)...
 	t2.Register(r, databaseUsers)
 	t.AddTopic(r, databaseTopics)
-	tmpl1.Execute(w, "")
+
+	tmpl1.Execute(w, t.TOPICS)
 }
---
+++ topics/displayTopic.go
@@ -0,0 +1,50 @@
+package forum
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Topics struct {
+	Id           int
+	Name         string
+	Likes        string
+	Dislikes     string
+	CreationDate string
+	Owner        string
+	Uuid         int
+}
+
+var TOPICS []Topics
+
+func DisplayTopic(db *sql.DB) {
+	name := ""
+	likes := ""
+	dislikes := ""
+	creationDate := ""
+	owner := ""
+	id := 0
+	row, err := db.Query("SELECT id, name, creationDate, owner, likes, dislikes from topics;")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for row.Next() {
+			err = row.Scan(&id, &name, &creationDate, &owner, &likes, &dislikes)
+			if err != nil {
+				fmt.Println(nil)
+			} else {
+				topicIndex := len(TOPICS)
+
+				TOPICS = append(TOPICS, Topics{})
+				TOPICS[topicIndex].Id = id
+				TOPICS[topicIndex].Name = name
+				TOPICS[topicIndex].Likes = likes
+				TOPICS[topicIndex].Dislikes = dislikes
+				TOPICS[topicIndex].CreationDate = creationDate
+				TOPICS[topicIndex].Owner = owner
+			}
+		}
+		fmt.Println(len(TOPICS))
+		row.Close()
+	}
+}
