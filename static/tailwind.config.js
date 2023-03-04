e8e216de7ac1f059aa1fa96547e50d05d21e58a7
m4ul <lucas.hanson@ynov.com>
2023-03-04 19:32:55 +0100

    updated topics and comments page

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         'color3': '#f33862',
         'color4': '#2C3639',
         'color5': '#676afb',
+        'newcolor1': '#ffffff',
+        'newcolor2': '#E5E7EB',
+        'newcolor3': '#2D2D2A',
+        'newcolor4': '#DB2955',
       backgroundImage: {
         'frontend': "url('https://i.pinimg.com/originals/14/fc/d1/14fcd189633936157ee354b6a092169d.png')",
         'footer-texture': "url('/img/footer-texture.png')",
